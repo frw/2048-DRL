7f211f789dcb5d85aa9d934326bd4d77b1de10cf
robertsychen <robertsychen@gmail.com>
2015-11-27 10:32:38 -0500

    jk again use this one

--- rl/neural_network.py
+++ rl/neural_network.py
@@ -41,7 +41,7 @@ class HiddenLayer(object):
         #edited_dropout_factors = np.ones(n_in) - ((input[1]) * dropout_factors)
         #lin_output = (0.5 + 0.5 * input[1]) * T.dot((edited_dropout_factors * input[0]), self.W) + self.b
 
-        #lin_output = T.dot(input, self.W) + self.b
+        lin_output = T.dot(input, self.W) + self.b
         self.output = (
             lin_output if activation is None
             else activation(lin_output)
@@ -229,7 +229,7 @@ for i in range(5000):
     h1, h2 = my_nn.update_model(tester,10.0)
     print h1
     print h2'''
-
+'''
 x_data3 = np.array([1,2,3,4,5])
 x_data2 = np.random.permutation(x_data3)
 x_data = np.tile(x_data2,(20,1))
@@ -248,5 +248,23 @@ for k in range(500000):
             print y_data[i]
             print "try by forward:"
             print result
-
+'''
+x_data3 = np.array([1,2,3,4,5])
+x_data2 = np.random.permutation(x_data3)
+x_data = np.tile(x_data2,(16,1))
+y_data = x_data2 * 3.0 #+ (np.random.normal(5) * 0.01)
+print y_data
+for k in range(500000):
+    for i in range(y_data.shape[0]):
+        next_step, n2 = my_nn.update_model(x_data[:,i], 2, y_data[i])
+        result = my_nn.use_model(x_data[:,i], 2)
+        if k % 1000 == 0:
+            print "cost:"
+            print next_step
+            print "prediction:"
+            print n2
+            print "actual:"
+            print y_data[i]
+            print "try by forward:"
+            print result
 
